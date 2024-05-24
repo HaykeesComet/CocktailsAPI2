29da329f05ca2a6269b0b63db15d162298756945
Hector Sevilla <haykees.comet@live.com>
2024-05-24 04:21:33 -0700

    Update CocktailsAPI2

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://iameych77:Budzz5510~!@localhost:5432/cocktail_database')
+    SECRET_KEY = os.getenv('SECRET_KEY', '44056c242169e92244116b3c59055e28')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgres://iameych77:Zkic3xh6o6rNpxqetnb78xdVh3uxG16y@dpg-cp7mv4md3nmc73buv780-a/cocktail_database_lw3l')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
